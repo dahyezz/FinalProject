9d13f5fc99388de916b2835ee2fbc70f7117f942
Youngjin Mo <creator.youngjin@gmail.com>
2019-07-16 19:43:57 +0900

    UsedDaoImpl 작성 및 Controller 이름 변경 list 임시 완성(아직 테스트못함)

--- UniTime/src/main/java/web/service/impl/UsedServiceImpl.java
+++ UniTime/src/main/java/web/service/impl/UsedServiceImpl.java
@@ -1,5 +1,33 @@
 package web.service.impl;
 
-public interface UsedServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import web.dao.face.UsedDao;
+import web.dto.UsedBoard;
+import web.service.face.UsedService;
+import web.util.Paging;
+
+@Service
+public class UsedServiceImpl implements UsedService {
+	
+	@Autowired UsedDao usedDao;
+	
+	@Override
+	public Paging getPage(int curPage) {
+		
+		int totalCount = usedDao.selectCntAll();
+		
+		int listCount = 10;
+		Paging paging = new Paging(totalCount, curPage, listCount);
+		
+		return paging;
+	}
+	
+	@Override
+	public List<UsedBoard> list(Paging paging) {
+		return usedDao.selectAll(paging);
+	}
 }
