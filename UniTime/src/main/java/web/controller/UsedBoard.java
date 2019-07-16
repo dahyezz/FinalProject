781544957d676f1aa10dce5a3ee770ee5199f8b9
Youngjin Mo <creator.youngjin@gmail.com>
2019-07-16 14:59:32 +0900

    list 구현중\serviceimpl 미작성으로 에러발생중

---
+++ UniTime/src/main/java/web/controller/UsedBoard.java
@@ -0,0 +1,41 @@
+package web.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import web.service.face.UsedService;
+import web.util.Paging;
+
+@Controller
+public class UsedBoard {
+	
+	// Logger 객체 생성 
+	private static final Logger logger
+	= LoggerFactory.getLogger(UsedBoard.class);
+	
+	@Autowired UsedService usedService;
+	
+	@RequestMapping(value="/used/list",
+			method=RequestMethod.GET)
+	public void list(
+			Model model,
+			@RequestParam(defaultValue="1") int curPage 
+			) {
+		
+		Paging paging = usedService.getPage(curPage);
+		
+		List<UsedBoard> boardList
+		= usedService.list(paging);
+		
+		model.addAttribute("list", boardList);
+				
+	}
+}
