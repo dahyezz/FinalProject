f166f1ca48e6a1b49641a54a407f8db32662602e
dahyezz <anwlrqlc@naver.com>
2019-08-01 17:04:23 +0900

    2019.08.01 다혜  - 메인페이지 수정(비로그인 상태)

--- UniTime/src/main/java/web/controller/MainController.java
+++ UniTime/src/main/java/web/controller/MainController.java
@@ -1,5 +1,7 @@
 package web.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,7 +14,24 @@ public class MainController {
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 	
 	@RequestMapping(value="/main", method=RequestMethod.GET)
-	public void main() { }
+	public String main(HttpSession session) {
+		
+//		boolean loginCheck = (boolean) session.getAttribute("login");
+		String loginCheck = (String) session.getAttribute("nick");
+		logger.info(loginCheck);
+		
+		if(loginCheck == null || loginCheck == "") {
+//			logger.info("test");
+			return "redirect:/main_no";
+		} else {
+			return "/main";
+		}
+	}
+	
+	@RequestMapping(value="/main_no", method = RequestMethod.GET)
+	public void main_no() { 
+//		logger.info("test");
+	}
 	
 	@RequestMapping(value="/info/termofservice", method=RequestMethod.GET)
 	public void termOfService() { }
