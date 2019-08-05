a1f817db62aa4a6890258de4926af257e609f914
BJH <bj00061@naver.com>
2019-08-05 14:54:06 +0900

    2019.08.05 범진현 메인페이지 구현중...

--- UniTime/src/main/java/web/controller/MainController.java
+++ UniTime/src/main/java/web/controller/MainController.java
@@ -1,23 +1,53 @@
 package web.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import web.service.face.MainService;
 
 @Controller
 public class MainController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	@Autowired MainService mainService;
 	
 	@RequestMapping(value="/main", method = RequestMethod.GET)
-	public void main(HttpSession session) { }
+	public void main(HttpSession session) { 
+	}
 	
 	@RequestMapping(value="/kg_main", method=RequestMethod.GET)
-	public void kg_main(HttpSession session) {
+	public void kg_main(Model model) {
+		
+		List freeboardNoticeList = mainService.freeboardNotice();
+		model.addAttribute("notice", freeboardNoticeList);
+//		logger.info("공지사항 : " + freeboardNoticeList);
+		
+		List freeboard = mainService.freeboard();
+		model.addAttribute("freeboard", freeboard);
+//		logger.info("자유게시판 : " + freeboard);
+		
+		List tastyboard = mainService.tastyBoard();
+		model.addAttribute("tastyboard", tastyboard);
+//		logger.info("테이스티 : " + tastyboard);
+		
+		List usedBoard = mainService.usedBoard();
+		model.addAttribute("usedboard", usedBoard);
+		
+		
+		
+		
+		
+		
+		
 
 	}
 	
